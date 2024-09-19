b647b8b7541c92eeddc2c0760672aec3b907a1cf
harsh dhobe <harshdhobe123@gmail.com>
2024-09-20 02:49:28 +0530

    add donate function

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -25,6 +25,12 @@ contract CrowdFunding {
         owner = payable(campaignOwner);
     }
 
+function donate() public payable {
+        require(requiredAmount > receivedAmount, "required amount fullfilled");
+        owner.transfer(msg.value);
+        receivedAmount += msg.value;
+        emit donated(msg.sender, msg.value, block.timestamp);
+    }
 
     
    
