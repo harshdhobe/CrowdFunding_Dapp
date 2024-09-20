95df20d307a8e9bcca4dd4d18052864026ecd13c
harsh dhobe <harshdhobe123@gmail.com>
2024-09-20 15:29:39 +0530

    add getDonators

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -55,6 +55,13 @@ contract CrowdFunding {
         
     }
 
+     function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
+    
+
+
+
    
 
     
